ed1fe511b5be341a41a2bc14f18642984b58c912
Robert Stitic <robert.stitic7@gmail.com>
2024-03-14 21:06:47 +0100

    add user auth and db models

--- app/models.py
+++ app/models.py
@@ -1,15 +1,67 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
+from app import db, login
+from flask_login import UserMixin
 
-class User(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+#class User(db.Model):
+#    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+#    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
+#                                                unique=True)
+#    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
+#                                             unique=True)
+#    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+#    role: so.Mapped[str] = so.mapped_column(sa.String(10), index=True, unique=True)
+#
+#    def __repr__(self):
+#        return '<User {}>'.format(self.username)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
                                                 unique=True)
-    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
-                                             unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    role = db.Column(db.String(20), nullable=False)
+    balance = db.Column(db.Float, nullable=False)
+    orders = db.relationship('Order', backref='user', lazy=True)
+    basket_items = db.relationship('BasketItem', backref='user', lazy=True)
 
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
+    @login.user_loader
+    def load_user(id):
+        return db.session.get(User, int(id))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+class Ticket(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    #quantity = db.Column(db.Integer, default=1, nullable=False)
+    orders = db.relationship('Order', secondary='order_ticket', backref='tickets', lazy=True)
+    basket_items = db.relationship('BasketItem', backref='ticket', lazy=True)
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    total_price = db.Column(db.Float, nullable=False)
+
+class OrderTicket(db.Model):
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), primary_key=True)
+    ticket_id = db.Column(db.Integer, db.ForeignKey('ticket.id'), primary_key=True)
+
+class BasketItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    ticket_id = db.Column(db.Integer, db.ForeignKey('ticket.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+
+#class Notification(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+#    message = db.Column(db.Text, nullable=False)
--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,60 @@
-from flask import render_template
-from app import app
-from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user, logout_user, login_required
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
+import sqlalchemy as sa
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
+    return render_template('index.html', title='Početna')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data)
+        )
+        if user is None or not user.check_password(form.password.data):
+            flash('Pogrešan username ili password')
+            return redirect(url_for('login'))
+        flash('Prijava zatražena za korisnika {}, zapamti={}'.format(
             form.username.data, form.remember_me.data))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('login.html', title='Prijava', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, role='customer', balance=0)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Čestitamo, sada ste registrirani korisnik!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Registracija', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/profile/<username>')
+@login_required
+def profile(username):
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    return render_template('profile.html', title='Moj profil', user=user)
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm(current_user.username)
