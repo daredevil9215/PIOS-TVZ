59619f4dade5b3d7183223025473e62f24655eed
Robert Stitic <robert.stitic7@gmail.com>
2024-04-06 10:48:14 +0200

    save firstname and lastname in db from registration form

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -33,7 +33,7 @@ def register():
         return redirect(url_for('main.index'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data, is_admin=False, balance=0)
+        user = User(firstname=form.firstname.data, lastname=form.lastname.data, username=form.username.data, is_admin=False, balance=0)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
